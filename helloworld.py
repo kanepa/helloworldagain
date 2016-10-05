007a0a8bb5eba3264734ec1adfc69ddbd465a8ff
Paul Kane <kanepa1977@gmail.com>
2016-10-05 16:49:31 +0100

    update good choice

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,34 @@
 
-name = raw_input("Hi what is your name? ")
-print "hello ," + name
-age = raw_input("what is your age")
-print "your are old, " + age
+# name = raw_input("Hi what is your name? ")
+# print "hello ," + name
+# age = raw_input("what is your age")
+# print "your are old, " + age
+# team = raw_input("What is your favourite team?")
+# print "Hello, %s!" % name
+# print "hello, {0}! so, you are {1} years old".format(name, age)
+# print "hello, {0}! so, your favourite team is {1}".format(name, team)
+# print "hello".capitalize()
+#
+# if name.endswith("luo"):
+#     print"cool name"
+# else:
+#     print "boring name"
+#
+# somevalue ="hello".
+
+
+name = raw_input("Hi what is your name?")
+print "Hello " +name.capitalize()
+
+game = raw_input("would you like to play a game(y/n)")
+print "Hello, {0},".format(name.capitalize(), game)
+
+if game.endswith("y"):
+    print"good choice"
+else:
+    print"bad choice"
+
+
+
+
+
